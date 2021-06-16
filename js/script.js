876bbfc890d2c698d29bfa81cd806b1212327c8b
Cujo1404 <arkl3828@gmail.com>
2021-06-16 20:50:29 +1200

    added about page

--- js/script.js
+++ js/script.js
@@ -17,3 +17,52 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("emailbtmDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(e) {
+    if (!e.target.matches('.emailbtm')) {
+    var emailbtmDropdown = document.getElementById("emailbtmDropdown");
+      if (emailbtmDropdown.classList.contains('show')) {
+        emailbtmDropdown.classList.remove('show');
+      }
+    }
+  }
+
+  /* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("phonebtmDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(e) {
+    if (!e.target.matches('.phonebtm')) {
+    var phonebtmDropdown = document.getElementById("phonebtmDropdown");
+      if (phonebtmDropdown.classList.contains('show')) {
+        phonebtmDropdown.classList.remove('show');
+      }
+    }
+  }
+
+  /* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("locationbtmDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(e) {
+    if (!e.target.matches('.locationbtm')) {
+    var locationbtmDropdown = document.getElementById("locationbtmDropdown");
+      if (locationbtmDropdown.classList.contains('show')) {
+        locationbtmDropdown.classList.remove('show');
+      }
+    }
+  }
