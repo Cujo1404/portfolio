6c43084fba530520737fa7505594e784400a7346
cujo1404 <arkl3828@gmail.com>
2021-08-14 23:04:34 +1200

    added document formatting for easy reading

--- js/script.js
+++ js/script.js
@@ -63,23 +63,25 @@ function goBack() {
   window.history.back();
 }
 
-var supports = (function() {
+var supports = (function () {
   var d = document.documentElement,
-      c = "ontouchstart" in window || navigator.msMaxTouchPoints;
+    c = "ontouchstart" in window || navigator.msMaxTouchPoints;
   if (c) {
-      d.className += " touch";
-      return {
-          touch: true
-      }
+    d.className += " touch";
+    return {
+      touch: true
+    }
   } else {
-      d.className += " no-touch";
-      return {
-          touch: false
-      }
+    d.className += " no-touch";
+    return {
+      touch: false
+    }
   }
 })();
 
-Qualtrics.SurveyEngine.addOnReady(function() {
+Qualtrics.SurveyEngine.addOnReady(function () {
   $('ScrollButton').hide();
-  setTimeout(function() { jQuery("#ScrollButton").show(); },10000);
-  });
+  setTimeout(function () {
+    jQuery("#ScrollButton").show();
+  }, 10000);
+});
